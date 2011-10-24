95c02a213f0b7e97ec8f92563f32ab3961931132
Christian Trabold <ct@192-168-0-28.dkd.lan>
2011-10-24 09:04:07 +0200

    Split mongodb attributes into seperate file

---
+++ attributes/mongodb.rb
@@ -0,0 +1,7 @@
+default["graylog2"]["mongodb"]["host"] = "localhost"
+default["graylog2"]["mongodb"]["port"] = 27017
+default["graylog2"]["mongodb"]["max_connections"] = 500
+default["graylog2"]["mongodb"]["database"] = "graylog2"
+default["graylog2"]["mongodb"]["auth"] = "false"
+default["graylog2"]["mongodb"]["user"] = "user"
+default["graylog2"]["mongodb"]["password"] = "password"
