27e64baa9fa48e5efaad9d78de75c474640ef0f7
Christian Trabold <christian.trabold@dkd.de>
2011-11-18 16:08:58 +0100

    Split cron settings into seperate files

---
+++ attributes/cron.rb
@@ -0,0 +1,4 @@
+default["graylog2"]["send_stream_alarms"] = true
+default["graylog2"]["send_stream_subscriptions"] = true
+default["graylog2"]["stream_alarms_cron_minute"] = "*/15"
+default["graylog2"]["stream_subscriptions_cron_minute"] = "*/15"
