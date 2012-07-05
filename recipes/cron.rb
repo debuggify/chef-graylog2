05d3dd8427dff4a5e752ca0f63854129b5a42abb
Christian Trabold <christian.trabold@dkd.de>
2012-07-06 03:35:46 +0700

    Disable output for cronlog

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -21,11 +21,11 @@
 cron "Graylog2 send stream alarms" do
   minute node["graylog2"]["stream_alarms_cron_minute"]
   action node["graylog2"]["send_stream_alarms"] ? :create : :delete
-  command "cd #{node["graylog2"]["basedir"]}/web && RAILS_ENV=production bundle exec rake streamalarms:send"
+  command "cd #{node["graylog2"]["basedir"]}/web && RAILS_ENV=production bundle exec rake streamalarms:send 1> /dev/null"
 end
 
 cron "Graylog2 send stream subscriptions" do
   minute node["graylog2"]["stream_subscriptions_cron_minute"]
   action node["graylog2"]["send_stream_subscriptions"] ? :create : :delete
-  command "cd #{node["graylog2"]["basedir"]}/web && RAILS_ENV=production bundle exec rake subscriptions:send"
+  command "cd #{node["graylog2"]["basedir"]}/web && RAILS_ENV=production bundle exec rake subscriptions:send 1> /dev/null"
 end
