bc744b8240ec6d3b32d658aed7ba2eba65671541
Christian Trabold <christian.trabold@dkd.de>
2012-07-06 07:25:09 +0700

    Replace single quotes with double quotes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default["graylog2"]["basedir"] = "/var/graylog2"
-default["graylog2"]["port"] = 514
-default["graylog2"]["gelf_listen_port"] = 12201
-default["graylog2"]["collection_size"] = 50000000
-default["graylog2"]["send_stream_alarms_email"] = nil
+default["graylog2"]["basedir"]                         = "/var/graylog2"
+default["graylog2"]["port"]                            = 514
+default["graylog2"]["gelf_listen_port"]                = 12201
+default["graylog2"]["collection_size"]                 = 50000000
+default["graylog2"]["send_stream_alarms_email"]        = nil
 default["graylog2"]["send_stream_subscriptions_email"] = nil
